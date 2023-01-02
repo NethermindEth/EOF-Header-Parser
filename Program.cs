a7f88d37d2cb72ef7feafd6d912e324263e1ba1d
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2023-01-02 16:32:21 +0100

    Clarified arguments usage

--- Program.cs
+++ Program.cs
@@ -3,18 +3,24 @@ using Nethermind.Evm.EOF;
 
 var spec = (ReleaseSpec)Shanghai.Instance;
 if (args.Length > 0) {
-    if((args[0] == "--deactivate" || args[0] == "-d") && args.Length > 1) {
+    if(args[0] == "--deactivate" || args[0] == "-d") {
+        if(args.Length < 2) {
+            Console.WriteLine("Usage: --deactivate <Eip-Number>+ \n\t Eip-Number must be an integer and must be a valid EIP number. \n\t EIP deactivates all EIPs that depend on it.");
+            return;
+        }
+
         foreach(var arg in args.Skip(1)) {
             spec = spec.DeactivateEip(int.Parse(arg));
         }
         Console.WriteLine($"Current Activated Eof Eips => 3540 : {spec.IsEip3540Enabled}, 3670 : {spec.IsEip3670Enabled}, 4200 : {spec.IsEip4200Enabled}, 4750 : {spec.IsEip4750Enabled}, 5450 : {spec.IsEip5450Enabled}");
     } else if(args[0] == "--diff") {
+        if(args.Length < 3) {
+            Console.WriteLine("Usage: --diff <Source-File> <Target-File>+ \n\t Source-File and Target-File must be a valid file path.");
+            return;
+        }
         DiffChecker.Check(args.Skip(1).ToArray());
         return;
-    } else {
-        Console.WriteLine("Usage: --deactivate <Eip-Number>+ \n\t Eip-Number must be an integer and must be a valid EIP number. \n\t EIP deactivates all EIPs that depend on it.");
-        return;
-    }
+    } 
 }
 
 EvmObjectFormat? parser = new EvmObjectFormat(spec);
--- README.md
+++ README.md
@@ -4,6 +4,7 @@ a simple parser of EOF headers from Shanghai compatible bytecodes
 # Usage : 
 * without cmd arguments : Assumes Full EOF
 * with cmd arguments : 
+  * Empty, takes a line from stdin and validates it against Full-Eof and gives result to stout
   * ``--deactivate|-d <Eip-Number>+``, Every EIP deactivated will deactivate the ones that depend on it
   * ``--diff <filepaths>+``, diffs results of different files
 # Example : 
